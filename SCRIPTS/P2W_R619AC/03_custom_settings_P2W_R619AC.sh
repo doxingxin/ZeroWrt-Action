aaaba21f4f724a18e811ec41f5e0fe462c7b1ee2
doxingxin <58932492+doxingxin@users.noreply.github.com>
2025-04-22 14:54:24 +0800

    Update 03_custom_settings_P2W_R619AC.sh

--- SCRIPTS/P2W_R619AC/03_custom_settings_P2W_R619AC.sh
+++ SCRIPTS/P2W_R619AC/03_custom_settings_P2W_R619AC.sh
@@ -2,7 +2,7 @@
 clear
 
 # 使用特定的优化
-sed -i 's,-mcpu=generic,-march=armv8-a+crc+crypto,g' include/target.mk
+sed -i 's/-mcpu=generic/-march=armv7-a+neon-vfpv4+crypto/g' include/target.mk
 sed -i 's,kmod-r8168,kmod-r8169,g' target/linux/rockchip/image/armv8.mk
 
 find ./ -name *.orig | xargs rm -f
